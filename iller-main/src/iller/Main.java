64f576766798a142b368f2f937bd003943a2098c
Muratibmtal <muratibmtal@gmail.com>
2021-09-28 23:38:55 +0300

    Initial commit

---
+++ iller-main/src/iller/Main.java
@@ -0,0 +1,101 @@
+package iller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String projectName="iller ve bölgeleri";  
+		System.out.println("Ýstanbul>>Marmara Bölgesi");
+		System.out.println("Sakarya>>Marmara Bölgesi");
+		System.out.println("Bursa>>Marmara Bölgesi");
+		System.out.println("Kocaeli>>Marmara Bölgesi");
+		System.out.println("Kýrklareli>>Marmara Bölgesi");
+		System.out.println("Edirne>>Marmara Bölgesi");
+		System.out.println("Çanakkale>Marmara Bölgesi");
+		System.out.println("Balýkesir>>Marmara Bölgesi");
+		System.out.println("Tekirdað>>Marmara Bölgesi");
+		System.out.println("Yalova>>Marmara Bölgesi");
+		System.out.println("   Karadeniz");
+		System.out.println("Tokat>>Karadeniz Bölgesi");
+		System.out.println("Samsun>>Karadeniz Bölgesi");
+		System.out.println("Sinop>>Karadeniz Bölgesi");
+		System.out.println("Bartýn>>Karadeniz Bölgesi");
+		System.out.println("Zonguldak>>Karadeniz Bölgesi");
+		System.out.println("Bolu>>Karadeniz Bölgesi");
+		System.out.println("Düzce>>Karadeniz Bölgesi");
+		System.out.println("Karabük>>Karadeniz Bölgesi");
+		System.out.println("Kastamonu>>Karadeniz Bölgesi");
+		System.out.println("Çorum>>Karadeniz Bölgesi");
+		System.out.println("Amasya>>Karadeniz Bölgesi");
+		System.out.println("Ordu>>Karadeniz Bölgesi");
+		System.out.println("Giresun>>Karadeniz Bölgesi");
+		System.out.println("Gümüþhane>>Karadeniz Bölgesi");
+		System.out.println("Trabzon>>Karadeniz Bölgesi");
+		System.out.println("Bayburt>>Karadeniz Bölgesi");
+		System.out.println("Rize>>Karadeniz Bölgesi");
+		System.out.println("Artvin>>Karadeniz Bölgesi");
+		System.out.println("  ÝÇ Anadolu Bölgesi");
+		System.out.println("Eskiþehir>>Ýç Anadolu Bölgesi");
+		System.out.println("Konya>>Ýç Anadolu Bölgesi");
+		System.out.println("Ankara>>Ýç Anadolu Bölgesi");
+		System.out.println("Aksaray>>Ýç Anadolu Bölgesi");
+		System.out.println("Çankýrý>>Ýç Anadolu Bölgesi");
+		System.out.println("Kýrýkkale>>Ýç Anadolu Bölgesi");
+		System.out.println("Kýrþehir>>Ýç Anadolu Bölgesi");
+		System.out.println("Yozgat>>Ýç Anadolu bölgesi");
+		System.out.println("Niðde>>Ýç Anadolu Bölgesi");
+		System.out.println("Nevþehir>>Ýç Anadolu Bölgesi");
+		System.out.println("Kayseri>>Ýç Anadolu Bölgesi");
+		System.out.println("Sivas>>Ýç Anadolu Bölgesi");
+		System.out.println("  Akdeniz Bölgesi");
+		System.out.println("Osmaniye>>Akdeniz Bölgesi");
+	    System.out.println("Adana>>Akdeniz Bölgei");
+	    System.out.println("Isparta>>Akdeniz Bölgesi");
+	    System.out.println("Antalya>>Akdeniz Bölgesi");
+	    System.out.println("Burdur>>Akdeniz :Bölgesi");
+	    System.out.println("Mersin>>Akdeniz Bölgesi");
+	    System.out.println("Hatay>>Akdeniz Bölgesi");
+	    System.out.println("Kahramanmaraþ>>Akdeniz Bölgesi");
+	    System.out.println(  "Eðe Bölgesi");
+	    System.out.println("Kütahya>>Ege Bölgesi");
+	    System.out.println("Denizli>>Ege Bölgesi");
+	    System.out.println("Muðla>>Ege Bölgesi");
+	    System.out.println("Ýzmir>>Ege Bölgesi");
+	    System.out.println("Aydýn>>Ege Bölgesi");
+	    System.out.println("Manisa>>Ege Bölgesi");
+	    System.out.println("Uþak>>Ege Bölgesi");
+	    System.out.println("Afyon>>Ege Bölgesi");
+	    System.out.println("  Doðu Anadolu Bölgesi");
+	    System.out.println("Iðdýr>>Doðu Anadolu Bölgesi");
+	    System.out.println("Ardahan>>Doðu Anadolu bölgesi");
+	    System.out.println("Kars>>Doðu Anadolu Bölgesi");
+	    System.out.println("Muþ>>Doðu Anadolu Bölgesip");
+	    System.out.println("Bingöl>>Doðu Anadolu Bölgesi");
+	    System.out.println("Elazýð>>Doðu Anadolu Bölgesi");
+	    System.out.println("Malatya>>Doðu Anadolu Bölgesi");
+	    System.out.println("Erzincan>>Doðu Anadolu Bölgesi");
+	    System.out.println("Tunceli>>Doðu Anadolu Bölgesi");
+	    System.out.println("Erzurum>>Doðu Anadolu Bölgesi");
+	    System.out.println("Bitlis>>Doðu Anadolu Bölgesi");
+	    System.out.println("Aðrý>>Doðu Anadolu Bölgesi");
+	    System.out.println("Hakkari>>Doðu Anadolu Bölgesi");
+	    System.out.println("  GüneyDoðu Anadolu Bölgesi");
+	    System.out.println("Þýrnak>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Siirt>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Mardin>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Diyarbakýr>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Þanlýurfa>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Adýyaman>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Gaziantep>>Güneydoðu Anadolu Bölgesi");
+	    System.out.println("Kilis>>Güneydoðu Anadolu Bölgesi"); 
+	    
+	    System.out.println("iller ve bölgeleri:"+projectName);
+	    
+	   
+	    
+	    
+		
+		
+	}
+
+}
